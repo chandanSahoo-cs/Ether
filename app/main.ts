fd287af2ae68020d7dd1325ee7dd365fd1c93b62
Chandan <chandansahoo02468@gmail.com>
2025-08-30 19:39:56 +0530

    Add ping

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,29 @@
 import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Starting Aero server...");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  console.log("Client connected");
+
+  connection.on("data", (data: Buffer) => {
+    const message = data.toString().trim();
+
+    console.log("Received: ", JSON.stringify(message));
+
+    if (message.includes("PING")) {
+      connection.write("+PONG\r\n");
+    } else {
+      connection.write("-ERR unknown command\r\n");
+    }
+  });
+
+  connection.on("end",()=>{
+    console.log("Client disconnected");
+  })
 });
 //
-server.listen(6379, "127.0.0.1");
+server.listen(6379, "127.0.0.1",()=>{
+    console.log("Server listening on 127.0.0.1:6379")
+});
